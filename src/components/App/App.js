1260dc3a70e074a7162cf685c612b8deba704227
Trix <89504082116@mail.ru>
2021-07-30 11:25:14 +0700

    add Routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Main from '../Main/Main';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Main/>
+            <Router>
+                
+
+                <Route exact path="/" component={ Main } />
+            </Router>
             <Footer/>
         </div>
     );
